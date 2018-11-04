036a4a3903c62b347f9f8f069231da8d300270d1
Unknown <andre.rhoden@upshootmedia.com>
2018-11-04 02:27:52 -0500

    updated content for sports/basketball

--- resources/views/publicportal/sports/basketball.blade.php
+++ resources/views/publicportal/sports/basketball.blade.php
@@ -21,21 +21,31 @@
             </div>
             <div class="col-md-8">
               
-               <h2>Changing the <span class="blue">Game</span></h2>
+            <div class="clearfix" id="body_content_left" style="min-height:1150px;">
+          		<p class="quote">In basketball, being able to rapidly change directions and move between forward and backward, side-to-side and up-down is a definite advantage. Discover what many of today’s professional athletes already know. With AgilityGuard™ on your side, the way you perform will never be the same again.</p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus enim, scelerisque ac ante sit amet, feugiat porttitor tellus. Sed quam nisl, venenatis sit amet venenatis sed, luctus sed justo. Donec nunc ante, lobortis et viverra eu, porta vitae mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin laoreet gravida bibendum. Phasellus maximus turpis justo, sit amet hendrerit lacus interdum eu. Duis nulla nisi, egestas ac tristique sed, luctus ac tortor. Quisque tincidunt lorem ut quam tempor, in gravida nisi pellentesque.
-                </p>
-                <p>
-                    Morbi faucibus lectus quis ex varius, a egestas orci eleifend. Nulla facilisi. Sed convallis urna ut nunc pretium, quis lacinia dui lacinia. Sed ut tempor urna. Duis luctus orci at mauris eleifend mollis. Mauris et massa accumsan, feugiat felis eu, porttitor nunc. Nam semper enim magna, at consequat sapien congue vel.
-                </p>
-                <h3 class="gold">Cras id sapien eu lectus accumsan feugiat.</h3>
-                <p>
-                     Maecenas lorem mauris, aliquam ac gravida a, egestas in neque. Phasellus fermentum, ante ac vehicula elementum, velit velit vestibulum risus, condimentum aliquet enim sem ac risus. Aenean ac purus tempor, semper metus eget, gravida tortor. Sed ut gravida ipsum. Duis vehicula ipsum vitae lacus auctor, eu egestas lacus volutpat. Sed vestibulum justo risus, non ultrices elit mattis non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce aliquet arcu in odio placerat tempus. Ut id neque id enim imperdiet varius ac vel felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas iaculis nunc vitae dui cursus, nec mattis risus pretium. Fusce sed magna sit amet metus tincidunt faucibus placerat id quam. Quisque nulla dui, rutrum non risus eget, consectetur tristique elit.
-                </p><p>
-                    Nulla placerat magna ut nisl ornare, eget ultrices lectus aliquet. Sed pretium erat felis, aliquet malesuada neque venenatis quis. Donec risus ante, lacinia et cursus sit amet, scelerisque eu mauris. Aenean ac malesuada tortor. Donec ac turpis lacinia, pretium tellus at, gravida massa. Quisque placerat tempus pharetra. Quisque id cursus ligula. Suspendisse et ligula id augue semper pretium. Morbi a ultricies ex. Nam sed tempor lectus. Nam nulla est, eleifend a tortor vel, suscipit auctor orci.
-                </p><p>
-                    Sed suscipit aliquet justo eu egestas. Aenean hendrerit consectetur egestas. Etiam eget nisl molestie, semper nibh id, faucibus massa. Pellentesque vehicula vestibulum mauris, ac malesuada odio blandit ut. Aliquam a dui lobortis, laoreet tellus a, aliquam ligula. Mauris fringilla quam at velit convallis, vitae sollicitudin velit scelerisque. Phasellus faucibus aliquam sollicitudin. Fusce sit amet urna neque. Phasellus ultricies diam in lacus pretium, eget blandit nisi ornare. Ut eget malesuada dui. Sed placerat sed nunc ultrices venenatis. Fusce porttitor, justo a facilisis mattis, arcu nisi tempus velit, id consectetur erat nisl nec quam. Mauris finibus sapien mauris, non sagittis mauris vehicula a. Mauris convallis sodales tellus consequat sollicitudin.</p>
+<h2>Strength and Power</h2>
+<p>Explosive power, particularly in your vertical jump, is key to you. This requires that you be strong. &nbsp;And every little bit helps. With Triumphant on your team, you’ll have an immediate advantage over your opponent from training day straight through to game day. </p>
 
+<h2>Speed and Acceleration</h2>
+<p>Calling upon your body to get away, and stay ahead, of your opponents gives you an edge. Accelerating, reacting, and competing faster than before. Improve your overall speed and reaction time and you’ll improve the way you play. AgilityGuard™ gives you the explosive edge you’ve always wanted.</p>
+<h2>Balance</h2>
+<p>Solid footwork depends on balance. When ball-handling, rebounding, blocking or defending, or attacking your opponents' net, it is crucial that you be able to maintain your balance and maintain control. With better balance you’ll step when you want to step, move when you want to move and stand your ground when it counts the most. Improved balance and alignment can mean increased accuracy, and a more consistent, centred performance.  </p>
+<h2>Agility</h2>
+<p>Being more agile means staying light on your toes and ready to make your move. Explode from a stand still, stop on a dime and take your game to a whole new level with a little help from AgilityGuard™. &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <br> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   </p>
+<h2>Endurance and Recovery</h2>
+<p>Your ability to play longer, run harder and increase your tolerance to take more pain gives you an advantage. With help from AgilityGuard™ you may be able to train harder, recover faster and improve your performance where it counts.  </p>
+<p>We do not have empirical evidence to support enhanced endurance and recovery, but have been told by users that they have found that they have better endurance and recover more quickly. These are areas that we would want to pursue for empirical evidence.</p>
+<h2>AgilityGuard Specific Experience and Expected Research</h2>
+<p>AgilityGuard users consistently report increased flexibility, balance, core strength and explosive power. A recent study published by Dr. Shaun Arents and others of Rutgers Univeristy. "Effects of a neuromuscular dentistry- designed mouthguard on muscular endurance and anaerobic power", Comparative Exercise Physiology, Cambridge University Press, 2010, suggests an enhancement in peak power output, performance and repeated maximal efforts by individuals using a mouthguard based on Dr Chan's techniques.  Triumphant is looking at pursuing formal research on the potential of the AgilityGuard to improve flexibility, balance, endurance, recovery, core strength and stability among other attributes.</p>
+<h3><em>Important to Note</em>
+</h3>
+<p><em>It is important to understand that each individual is different, and that only you can determine whether an AgilityGuard would provide any potential improvement in your athletic performance. This is best determined by you visiting an AgilityGuard dentist nearest you and having him or her perform the Agility Test on you. Only then should you make a decision to purchase an AgilityGuard.</em>
+</p>
+
+          
+          </div>
+          
             </div>
           </div>
           <div class="row callouts">
