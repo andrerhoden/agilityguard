636ab63b29a8e72bde9960a03a152c89ac66db01
Unknown <andre.rhoden@upshootmedia.com>
2018-11-16 01:33:51 -0500

    created model for DentalPractice

---
+++ app/DentalPractice.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class DentalPractice extends Model
+{
+    protected $table = 'DentalPractices';
+
+    // public $additional_attributes = ['Name'];
+
+    // public function getNameAttribute()
+    // {
+    //     return "{$this->Name}";
+    // }
+
+}
