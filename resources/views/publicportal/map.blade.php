13d4c9fc1e13a0bb56878c94faa63017aeb5ce3a
Jessica Merrigan <jessica@thepixelshop.ca>
2018-11-19 23:55:48 -0500

    test google map

--- resources/views/publicportal/map.blade.php
+++ resources/views/publicportal/map.blade.php
@@ -1,75 +1,44 @@
 @extends('layouts.publicportal.main')
 @section('content')
 
-  
-  <section class="masthead white-bg">
-          <div class="container">
-            <div class="row">
-              <div class="col">
-                <h1>About Us</h1>
-              </div>
-            </div>
-          </div>
-      </section>
-        
-
-      <section class="white-bg">
-        <div class="container">
-          
-          <div class="row">
-            <div class="col">
-              
-
-            
-              <h2>Changing the <span class="blue">Game</span></h2>
-              <h2>Agility</h2>
-              <p><img src="http://www.agilityguard.com/assets/images/system/images/0000/0931/ApoloOhno2_medium.jpg" title="Apolo Ohno2" alt="Apolo Ohno2" rel="medium" class="rounded float-left" /></p>
-              <p>As 8-time Olympic medalist Apolo Anton Ohno will attest, every athlete, player, trainer, coach and fitness enthusiast is aware of the benefits of being "agile". Agility is defined as the ability to change the body's position efficiently, requiring the integration of isolated movement skills using a combination of balance, co-ordination, speed, reflexes, strength, endurance and stamina.&nbsp;</p>
-              
-              <h3 class="clearfix">Not a New Idea - Just one who's time has come!&nbsp;</h3>
-              <p>The potential to improve athletic performance by using a properly designed mouthguard <strong><em>is not a new idea</em> </strong>. Dentists have, for some 40 years, noted improvements in athletic performance when athletes used a properly designed mouthguard.&nbsp;</p>
-              <p>Still, for many, wearing a mouthguard while playing contact sports is viewed simply as a way to protect the teeth. Football Players, Boxers, Hockey Players, MMA Athletes and others can all be seen wearing (and often chewing on) their mouthguard on the field, on the ice and in the ring.&nbsp;It used to be that if you played a sport where you could get hit, you wear a mouthguard - any mouthguard - plain and simple and no thought required.</p>
-              <p>While few serious athletes would consider competing with the wooden racquets of the 1970s, the shoes of bygone eras or outdated equipment in general, it is not uncommon to see today's athletes using mouthguards little changed from their parents' competitive days. Surely, there must be a better way!</p>
-              <h3>Now there's AgilityGuard: Performance Protection</h3>
-              <p><img src="http://www.agilityguard.com/assets/images/system/images/0000/2461/AgilityGuard_-_Performance_Protection_large.png" title="Agility Guard   Performance Protection" alt="Agility Guard   Performance Protection" rel="large" class="rounded mx-auto d-block" /></p>
-              <p>Technological advancements have in fact redefined the importance and role of mouthguards in sports.&nbsp;With a focus on precision, durability and comfort, each AgilityGuard is truly custom fit to unleash the performance potential of the most important athlete in the world - You!</p>
-              <p>Available only through a network of Certified AgilityGuard dentists with substantial collective experience assisting world-class, professional, Olympic athletes in addition to everyday athletes, you can be sure that your AgilityGuard is fabricated to the highest standards with a relentless focus on performance excellence. &nbsp; &nbsp;&nbsp;</p>
-              <h3 class="gold"><em>Important to Note</em></h3>
-              <p><em>It is important to understand that each individual is different, and that only you can determine whether an AgilityGuard would provide any potential improvement in your athletic performance. This is best determined by you visiting an AgilityGuard dentist nearest you and having him or her perform the Agility Test on you. Only then should you make a decision to purchase an AgilityGuard.</em> </p>
+<style>
+/* Always set the map height explicitly to define the size of the div
+ * element that contains the map. */
+#map {
+  height: 100%;
+  min-height: 400px;
+}
+/* Optional: Makes the sample page fill the window. */
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
 
 
-              <hr/>
-           
-            </div>
-          </div>
+<div id="map"></div>
+<!-- Replace the value of the key parameter with your own API key. -->
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7aEgrvcIl_h6cLGOVDdA1C1nc4jUthNw&callback=initMap">
+</script>
 
+<script>
+function initMap() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
 
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
 
-
-
-          <div class="row callouts">s
-              <div class="callout col-md-3">
-                <img src="img/callouts/Mouthguard-Lower-Grey-thumbnail.jpg" />
-              </div>
-              <div class="callout col-md-9">
-              
-               
-                  
-                    <h4>Facts from the American Dental Association</h4>
-                    <ul>
-                      <li>A properly fitted mouthguard reduces the chances of sustaining a concussion from a blow to the jaw.</li>
-                      <li>Mouthguards should be worn at all times during competition, in practice as well as in games.</li>
-                      <li>The American Dental Association recommends wearing custom mouthguards for the following sports: acrobats, basketball, boxing, field hockey, football, gymnastics, handball, ice hockey, lacrosse, racquetball, martial arts, racquetball, roller hockey, rugby, shot put, skateboarding, skiing, skydiving, soccer, squash, surfing, volleyball, water polo, weightlifting, wrestling.</li>
-                    </ul>
-                  
-                
-              </div>
-            </div>
-          </div>
-         
-        </div>
-     
-      </section>
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+</script>
 
 
 @endsection
