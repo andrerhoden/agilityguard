1c060f1744f4d12f2c7bf760121e6900e5ec0479
Unknown <andre.rhoden@upshootmedia.com>
2018-11-24 16:38:18 -0500

    Geocoding API to fetch contacts for dp

--- app/DentalPractice.php
+++ app/DentalPractice.php
@@ -9,12 +9,17 @@ class DentalPractice extends Model
 {
     protected $table = 'DentalPractices';
 
-    protected $fillable = ['Name', 'BillingAdress', 'EmailAddress', 'Description', 'Lat', 'Long'];
+    // protected $fillable = ['Name', 'BillingAdress', 'EmailAddress', 'Description', 'Lat', 'Long'];
     // public $additional_attributes = ['Name'];
 
     // public function getNameAttribute()
     // {
     //     return "{$this->Name}";
     // }
+
+    public function Contacts()
+    {
+        return $this->hasMany('App\Contact', 'dental_practice_id', 'id');
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-
-
-Route::get('/fetchMapDentalPractices/{key}/{search}', 'PublicPortalAjaxController@fetchMapDentalPractices');
+Route::get('/fetchMapDentalPractices/{key}/{search}/{distance}/{unit}', 'PublicPortalAjaxController@fetchMapDentalPractices');
