8a68ed7e7b186f270deb66df4d66d989ff3fa970
Unknown <andre.rhoden@upshootmedia.com>
2019-01-14 15:31:29 -0500

    corrected main logo link

--- resources/views/layouts/publicportal/main.blade.php
+++ resources/views/layouts/publicportal/main.blade.php
@@ -32,7 +32,7 @@
   <header>
 
       <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="/">
+        <a class="navbar-brand" href="{{$_ENV['APP_URL']}}">
           <img src="{{$_ENV['APP_URL']}}img/logo.png" alt="{{ setting('site.title') }}" />
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
